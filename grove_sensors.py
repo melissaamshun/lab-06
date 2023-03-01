1c9d93a022ab13b59a88ce59813fde09e2a6fa88
Donya82 <123035772+Donya82@users.noreply.github.com>
2023-02-28 17:24:37 -0800

    Update grove_sensors.py

--- grove_sensors.py
+++ grove_sensors.py
@@ -17,3 +17,63 @@ while True:
         print ("Error:{}".format(e))
     
     time.sleep(0.1) # don't overload the i2c bus
+
+    
+# Connect the Grove Rotary Angle Sensor to analog port A0
+# SIG,NC,VCC,GND
+potentiometer = 0
+
+# Connect the LED to digital port D5
+# SIG,NC,VCC,GND
+led = 5
+
+grovepi.pinMode(potentiometer,"INPUT")
+grovepi.pinMode(led,"OUTPUT")
+time.sleep(1)
+
+# Reference voltage of ADC is 5v
+adc_ref = 5
+
+# Vcc of the grove interface is normally 5v
+grove_vcc = 5
+
+# Full value of the rotary angle is 300 degrees, as per it's specs (0 to 300)
+full_angle = 300
+
+while True:
+    try:
+        # Read sensor value from potentiometer
+        sensor_value = grovepi.analogRead(potentiometer)
+
+        # Calculate voltage
+        voltage = round((float)(sensor_value) * adc_ref / 1023, 2)
+
+        # Calculate rotation in degrees (0 to 300)
+        degrees = round((voltage * full_angle) / grove_vcc, 2)
+
+        # Calculate LED brightess (0 to 255) from degrees (0 to 300)
+        brightness = int(degrees / full_angle * 255)
+
+        # Give PWM output to LED
+        grovepi.analogWrite(led,brightness)
+
+        print("sensor_value = %d voltage = %.2f degrees = %.1f brightness = %d" %(sensor_value, voltage, degrees, brightness))
+    except KeyboardInterrupt:
+        grovepi.analogWrite(led,0)
+        break
+    except IOError:
+        print ("Error")
+        
+        
+        from grove_rgb_lcd import *
+
+setText("Hello world\nLCD test")
+setRGB(0,128,64)
+
+# Slowly change the colors every 0.01 seconds.
+for c in range(0,255):
+    setRGB(c,255-c,0)
+    time.sleep(0.01)
+
+setRGB(0,255,0)
+setText("Bye bye, this should wrap")
